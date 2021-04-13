9dbd5e31c616c754f74a268305df48cc9ddd1ed5
h8handles <h8handles@gmail.com>
2021-04-13 10:47:39 -0400

    added start point to google scrape from dork result

--- PyDorks.py
+++ PyDorks.py
@@ -1,21 +1,19 @@
-from requests_html import HTMLSession
+import requests 
+import time
+from bs4 import BeautifulSoup
 
 
+#headers = {'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0'}
 
-text = 'site:* intitle:"Component Browser Login"'
+
+text = 'inurl:"/lib/editor/atto/plugins/managefiles/" | inurl:"calendar/view.php?view=month"'
 url = 'https://www.google.com/search?q=' + text
+headers = {'User-Agent': 'Mozilla/5.0'}
 
 
-s = HTMLSession()
-r = s.get(url)
 
-r.html.render(sleep=1)
+r = requests.get(url, headers=headers)
+print(r.text)
 
-heads = r.html.xpath('/html/body/div[7]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div/div[1]/a/div/cite', first=True)
-
-print(heads)
-
-'''links = heads.find('/div/cite')
-print(links)
-for item in links:
-	print(item)'''
+soup = BeautifulSoup(r.text, 'html.parser')
+print(soup.prettify())
